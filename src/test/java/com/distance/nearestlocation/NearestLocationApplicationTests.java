4470a9ee6519f82ab2352c0e135eb1e9dc8c12db
Abhishek Anand <anandabhi.anand023@gmail.com>
2021-07-22 22:01:55 +0530

    first commit

---
+++ src/test/java/com/distance/nearestlocation/NearestLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distance.nearestlocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearestLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
